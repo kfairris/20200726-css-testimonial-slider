8fa452fbcb4437757f1837406e362b889d89c967
Kenny Fairris <kfairris@gmail.com>
2020-07-28 09:42:27 -0500

    initial

---
+++ custom.js
@@ -0,0 +1,21 @@
+let slider = tns({
+    container: '.hero__slider',
+    items: 1,
+    slideBy: 'page',
+    // autoWidth: true,
+    center: true,
+    mouseDrag: false,
+    // arrowKeys: true,
+    // autoplay: false,
+    loop: false,
+    // fixedWidth: 1440,
+    controls: true,
+    controlsContainer: '#customize-controls',
+    // prevButton: '.prev',
+    // nextButton: '.next',
+    nav: false,
+    navPosition: 'bottom',
+    // swipeAngle: false,
+    speed: 400,
+    // viewportMax:1440
+});
